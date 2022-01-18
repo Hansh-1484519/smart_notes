9ca0299e10207a4f51a8f8ec25fb2f77e30c8a21
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2022-01-18 16:42:07 +0530

    added dom manipulation to add notes

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const input = document.querySelector('#content');
 const list = document.querySelector('.add')
 const Add = document.getElementById('submit');
+const note = document.getElementById('note');
 const AddTask = () => {
     const newitem = document.createElement('li');
     newitem.textContent = input.value;
     list.appendChild(newitem);
 }
-
+const OpenNote = () =>{
+    const textBox = document.createElement('textarea');
+    const btn = document.createElement('button');
+    list.appendChild(textBox);
+    list.appendChild(btn);
+}
+note.addEventListener('click', OpenNote);
 Add.addEventListener('click' , AddTask);
