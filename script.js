d6e6001d0efe88cfbe8b5f1265f12195efe1a577
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2022-01-18 18:02:01 +0530

    most basic notes app created

--- script.js
+++ script.js
@@ -2,16 +2,40 @@ const input = document.querySelector('#content');
 const list = document.querySelector('.add')
 const Add = document.getElementById('submit');
 const note = document.getElementById('note');
+const box = document.querySelector('.box');
+
+/*
 const AddTask = () => {
     const newitem = document.createElement('li');
     newitem.textContent = input.value;
     list.appendChild(newitem);
 }
+*/
 const OpenNote = () =>{
     const textBox = document.createElement('textarea');
-    const btn = document.createElement('button');
-    list.appendChild(textBox);
-    list.appendChild(btn);
+    var save = document.createElement('button');
+    save.textContent = 'save';
+    box.appendChild(textBox);
+    box.appendChild(save);
+    note.disabled = true;
+
+
+    const postNote = () =>{
+        if(textBox.value){
+            note.disabled = false;
+            const content = textBox.value;
+           const Area = document.createElement('p');
+           Area.classList = 'area';
+           Area.textContent = content;
+           list.appendChild(Area);
+            box.removeChild(textBox);
+            box.removeChild(save);
+           // list.innerHTML = textBox.value;
+        }
+    }
+    save.addEventListener('click' , postNote);
 }
+
+
 note.addEventListener('click', OpenNote);
 Add.addEventListener('click' , AddTask);
